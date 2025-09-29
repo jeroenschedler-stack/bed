814f9a383a180ed42c2d93333ec20a28375723ee
jeroenschedler-stack <jeroen.schedler@gmail.com>
2025-09-30 04:29:50 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,2 @@
-// sw.js (in /bed/)
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
